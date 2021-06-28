8bd082fbeb875dc44e8ea746acf53169f8c028df
ahmad-ahmad <aa0796780751@gmail.com>
2021-06-28 22:13:19 +0300

    add lab31

--- src/components/todo/todo-connected.js
+++ src/components/todo/todo-connected.js
@@ -1,33 +1,56 @@
 import React, { useEffect, useState } from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
+import axios from 'axios'; 
+
+import useAjax from '../../hooks/useAjax';
 
 import './todo.scss';
 
 const todoAPI = 'https://api-js401.herokuapp.com/api/v1/todo';
 
 
-const ToDo = () => {
+const ToDo1 = () => {
 
+  // const [list, _addItem,_toggleComplete,_getTodoItems,deleteItem,editItem] = useAjax(todoAPI)
+  let [apiHandler] = useAjax();
   const [list, setList] = useState([]);
-
-  const _addItem = (item) => {
+  
+  const  _addItem =  async (item) => {
     item.due = new Date();
-    fetch(todoAPI, {
-      method: 'post',
-      mode: 'cors',
-      cache: 'no-cache',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(item)
-    })
-      .then(response => response.json())
-      .then(savedItem => {
-        setList([...list, savedItem])
-      })
-      .catch(console.error);
+    // fetch(todoAPI, {
+    //   method: 'post',
+    //   mode: 'cors',
+    //   cache: 'no-cache',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify(item)
+    // })
+    //   .then(response => response.json())
+    //   .then(savedItem => {
+    //     setList([...list, savedItem])
+    //   })
+    // axios({
+    //   method: 'post',
+    //   url: todoAPI,
+    //   mode: 'cors',
+    //   cache: 'no-cache',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   data: JSON.stringify(item),
+    // })
+
+    try {
+      let savedItem = await apiHandler(todoAPI, "post", item);
+      setList([...list, savedItem.data]);
+    } catch (error) {
+      console.error(error.message);
+    }
+    // .then(savedItem => {
+    //   setList([...list, savedItem.data])
+    // })
+    //   .catch(console.error);
   };
 
-  const _toggleComplete = id => {
+  const _toggleComplete =async (id) => {
 
     let item = list.filter(i => i._id === id)[0] || {};
 
@@ -37,32 +60,186 @@ const ToDo = () => {
 
       let url = `${todoAPI}/${id}`;
 
-      fetch(url, {
-        method: 'put',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(item)
-      })
-        .then(response => response.json())
-        .then(savedItem => {
-          setList(list.map(listItem => listItem._id === item._id ? savedItem : listItem));
-        })
-        .catch(console.error);
+      // fetch(url, {
+      //   method: 'put',
+      //   mode: 'cors',
+      //   cache: 'no-cache',
+      //   headers: { 'Content-Type': 'application/json' },
+      //   body: JSON.stringify(item)
+      // })
+      //   .then(response => response.json())
+      //   .then(savedItem => {
+      //     setList(list.map(listItem => listItem._id === item._id ? savedItem : listItem));
+      //   })
+      // axios({
+      //   method: 'put',
+      //   url: url,
+      //   mode: 'cors',
+      //   cache: 'no-cache',
+      //   headers: { 'Content-Type': 'application/json' },
+      //   data: JSON.stringify(item),
+      // })
+      // .then(response => response.json())
+        // .then(savedItem => {
+        //   setList(list.map(listItem => listItem._id === item._id ? savedItem.data: listItem));
+        // })
+        // .catch(console.error);
+        try {
+          let savedItem = await apiHandler(url, "put", item);
+          setList(
+            list.map((listItem) =>
+              listItem._id === item._id ? savedItem.data : listItem
+            )
+          );
+        } catch (error) {
+          console.error(error.message);
+        }
     }
   };
 
-  const _getTodoItems = () => {
-    fetch(todoAPI, {
-      method: 'get',
-      mode: 'cors',
-    })
-      .then(data => data.json())
-      .then(data => setList(data.results))
-      .catch(console.error);
+
+
+  const _getTodoItems =async () => {
+    // fetch(todoAPI, {
+    //   method: 'get',
+    //   mode: 'cors',
+    // })
+    // axios({
+    //   method: 'get',
+    //   url: todoAPI,
+    //   // data: body !=='' ? JSON.parse(body) :body = {},                    
+     
+      
+    //   })
+     
+    //   // .then(data =>  data.json())
+    //   .then(data => setList(data.data.results) )
+    //   .catch(console.error);
+
+    try {
+      let myData = await apiHandler(todoAPI, "get");
+      setList(myData.data.results);
+    } catch (error) {
+      console.error(error.message);
+    }
   };
 
+  const deleteItem = async (id) => {
+    let item = list.filter(i => i._id === id)[0] || {};
+
+    if (item._id) {
+
+      item.complete = !item.complete;
+
+      let url = `${todoAPI}/${id}`;
+
+      // fetch(url, {
+      //   method: 'delete',
+      //   mode: 'cors',
+      //   cache: 'no-cache',
+      //   headers: { 'Content-Type': 'application/json' },
+      //   body: JSON.stringify(item)
+      // })
+
+      /*---------------------------*/
+      // axios({
+      //   method: 'delete',
+      //   url: url,
+      //   mode: 'cors',
+      //   cache: 'no-cache',
+      //   headers: { 'Content-Type': 'application/json' },
+      //   data: JSON.stringify(item),
+      // })
+      //   // .then(response => response.json())
+      //   .then(data => {
+      //     console.log(data,'ssssssssssss')
+      //     // setList(list.map(listItem => listItem._id === item._id ? savedItem : listItem));
+
+      //     let myId = list.indexOf(item)
+      //   console.log('ahmad')
+      //    console.log(list,'33333')
+      //     let myList = [...list]
+      //     myList.splice(myId, 1);
+      //     console.log(list,"jj")
+      //     setList(myList);
+      //   })
+        /*--------------------------------*/
+         try {
+        await apiHandler(url, "delete", item);
+        let myId = list.indexOf(item)
+        
+          let myList = [...list]
+          myList.splice(myId, 1);
+        
+          setList(myList);
+      } catch (error) {
+        console.error(error.message);
+      }
+        // .catch(console.error);
+        
+    }
+  }
+
+  const editItem = async (id,value)=> {
+    let item = list.filter(i => i._id === id)[0] || {};
+
+    if (item._id) {
+
+      item.text = value;
+
+      item.complete = !item.complete;
+
+      let url = `${todoAPI}/${id}`;
+
+      // fetch(url, {
+      //   method: 'put',
+      //   mode: 'cors',
+      //   cache: 'no-cache',
+      //   headers: { 'Content-Type': 'application/json' },
+      //   body: JSON.stringify(item)
+      // })
+      //   .then(response => response.json())
+      //   .then(savedItem => {
+      //     setList(list.map(listItem => listItem._id === item._id ? savedItem : listItem));
+      //   })
+      /**------------------------------- */
+      // axios({
+      //   method: 'put',
+      //   url: url,
+      //   mode: 'cors',
+      //   cache: 'no-cache',
+      //   headers: { 'Content-Type': 'application/json' },
+      //   data: JSON.stringify(item),
+      // })
+      // .then(response => response.json())
+      //   .then(savedItem => {
+      //     setList(list.map(listItem => listItem._id === item._id ? savedItem : listItem));
+      //   })
+      // // .then(savedItem => {
+      // //   setList([...list, savedItem.data])
+      // // })
+      // // .then(savedItem => {
+      // //       setList(list.map(listItem => listItem._id === item._id ? savedItem : listItem));
+      // //     })
+      //   .catch(console.error);
+      /**------------------------------- */
+
+      apiHandler(url, "put", item)
+      .then((savedItem) => {
+        setList(
+          list.map((listItem) =>
+            listItem._id === item._id ? savedItem.data : listItem
+          )
+        );
+      })
+      .catch(console.error);
+  }
+    }
+ 
+  
+
   useEffect(_getTodoItems, []);
+
 
   return (
     <>
@@ -82,6 +259,8 @@ const ToDo = () => {
           <TodoList
             list={list}
             handleComplete={_toggleComplete}
+            handleDelete = {deleteItem}
+            handleEdit = {editItem}
           />
         </div>
       </section>
@@ -89,4 +268,4 @@ const ToDo = () => {
   );
 };
 
-export default ToDo;
+export default ToDo1;
